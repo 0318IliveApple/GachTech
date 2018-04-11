fd0d72d193edcfcb11a4c8ca3607993464dae329
Hiroya NAKAJIMA <hiroya.nakajima@gmail.com>
2018-04-11 15:45:04 +0900

    完成

--- GachTech/ResultViewController.swift
+++ GachTech/ResultViewController.swift
@@ -52,6 +52,23 @@ class ResultViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(true)
+        let animation = CABasicAnimation(keyPath: "transform")
+        
+        animation.fromValue = NSNumber(value:0)
+        
+        animation.toValue = NSNumber(value:2 * Double.pi)
+        
+        animation.valueFunction = CAValueFunction(name: kCAValueFunctionRotateZ)
+        
+        animation.duration = 5
+        
+        animation.repeatCount = Float.infinity
+        
+        haikeiImageView.layer.add(animation, forKey: nil)
+    }
+    
 
     /*
     // MARK: - Navigation
